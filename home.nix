59d74438a38837aa975087ec753d4449481f36b1
StevenBuglione <stevenbuglione1@gmail.com>
2024-11-02 22:22:47 -0400

    first commit

---
+++ home.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "sbuglione";
+  home.homeDirectory = "/home/sbuglione";
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+  nixpkgs.config.allowUnfree = true;
+
+  imports = [
+    ./apps/git/git.nix
+  ];
+
+  home.packages = [
+    pkgs.neovim
+    pkgs.curl
+    pkgs.xclip
+    pkgs.fastfetch
+    pkgs.bash-completion
+    pkgs.tree
+    pkgs.fontconfig
+    pkgs.starship
+    pkgs.zoxide
+    pkgs.fzf
+    pkgs.multitail
+    pkgs.bat
+  ];
+
+  home.file = {
+    ".bashrc".source = ./apps/bash/.bashrc;
+    ".config/fastfetch/config.jsonc".source = ./apps/fastfetch/config.jsonc;
+    ".config/starship.toml".source = ./apps/starship/starship.toml;
+    ".config/kitty/kitty.conf".source = ./apps/kitty/kitty.conf;
+    ".local/share/fonts/JetbrainsMono".source = ./apps/fonts/JetbrainsMono;
+    ".ssh/config".source = ./apps/ssh/config;
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/sbuglione/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
